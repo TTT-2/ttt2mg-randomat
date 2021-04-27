fa8f6252a4343a54e5474321337a8e4d9263fa24
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2021-04-27 16:37:13 -0400

    Added commented out strings to be translated

--- lua/terrortown/lang/de/randomat.lua
+++ lua/terrortown/lang/de/randomat.lua
@@ -3,3 +3,7 @@ L = LANG.GetLanguageTableReference("de")
 L["ttt2_weapon_randomat_name"] = "Randomat MG"
 L["ttt2_weapon_randomat_desc"] = "Nutze diese Waffe um ein zufälliges Minispiel für diese Runde zu starten."
 L["ttt2_weapon_randomat_help_msb1"] = "Führt ein zufälliges Minispiel aus"
+
+--EVENT STRINGS
+-- L["title_mg_randomat"] = "A Randomat was activated"
+-- L["desc_mg_randomat"] = "{name} activated a Randomat."
